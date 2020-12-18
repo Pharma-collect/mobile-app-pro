24e4f61a4bff5a95743e24c0bd4fdb97d010e639
melvinmarra <melvin.marra@gmail.com>
2020-12-18 16:18:58 +0100

    Splash and login

---
+++ app/src/main/java/fr/isen/emelian/pharma_collect_pro/LoginActivity.kt
@@ -0,0 +1,79 @@
+package fr.isen.emelian.pharma_collect_pro
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.view.animation.AnimationUtils
+import android.widget.EditText
+import android.widget.Toast
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.VolleyError
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import fr.isen.emelian.pharma_collect_pro.services.EnableHttps.handleSSLHandshake
+import org.json.JSONObject
+
+class LoginActivity : AppCompatActivity() {
+
+    private var username_tf: EditText? = null
+    private var password_tf: EditText? = null
+    var backUrl = "https://88-122-235-110.traefik.me:61001/api/"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        val btt = AnimationUtils.loadAnimation(this, R.anim.down_to_up)
+        val layout = findViewById<ConstraintLayout>(R.id.layout)
+        layout.startAnimation(btt)
+
+    }
+
+    private fun logRequest(username: String, password: String){
+        val requestQueue = Volley.newRequestQueue(this)
+        val url = "$backUrl/api/user_pro/loginPro"
+        val stringRequest: StringRequest =
+            object : StringRequest(Request.Method.POST, url, object : Response.Listener<String?> {
+                override fun onResponse(response: String?) {
+                    var jsonResponse: JSONObject = JSONObject(response)
+                    if (jsonResponse["success"] == true) {
+                        intent = Intent(this@LoginActivity, MainActivity::class.java)
+                        startActivity(intent)
+                    }else{
+                        Toast.makeText(this@LoginActivity, "Wrong username or password", Toast.LENGTH_LONG).show()
+                    }
+                }
+            }, object : Response.ErrorListener {
+                override fun onErrorResponse(error: VolleyError) {
+                    Toast.makeText(this@LoginActivity, error.toString(), Toast.LENGTH_LONG)
+                        .show()
+                }
+            }) {
+                override fun getHeaders(): Map<String, String> {
+                    val params: MutableMap<String, String> = HashMap()
+                    params["Host"] = "node"
+                    return params
+                }
+
+                override fun getParams(): Map<String, String> {
+                    val params: MutableMap<String, String> = HashMap()
+                    params["username"] = username
+                    params["password"] = password
+
+                    return params
+                }
+            }
+        requestQueue.add(stringRequest)
+    }
+
+    fun onValidateClicked(view: View) {
+        handleSSLHandshake()
+        username_tf = findViewById<View>(R.id.etUsername) as EditText
+        password_tf = findViewById<View>(R.id.etPassword) as EditText
+        logRequest(username_tf!!.text.toString(), password_tf!!.text.toString())
+    }
+
+}
---
+++ app/src/main/java/fr/isen/emelian/pharma_collect_pro/SplashActivity.kt
@@ -0,0 +1,46 @@
+package fr.isen.emelian.pharma_collect_pro
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+import android.content.Intent
+import android.os.Handler
+import android.view.WindowManager
+
+
+class SplashActivity : AppCompatActivity() {
+
+    var charSequence: CharSequence? = null
+    var index = 0
+    var delay: Long = 200
+    var handler = Handler()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        //Get windows size fully fill
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+        // Switching Activity at the end
+        Handler().postDelayed({
+            startActivity(
+                Intent(this@SplashActivity, LoginActivity::class.java)
+                    .setFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            )
+//            customType(this@SplashActivity, "bottom-to-up")
+            finish()
+        }, 4000)
+    }
+
+    var runnable: Runnable = object : Runnable {
+        override fun run() {
+            if (index < charSequence!!.length) {
+                handler.postDelayed(this, delay)
+            }
+        }
+    }
+}
